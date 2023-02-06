de75fb6316e97f38b06d60a0ce952a1233d0856b
Moc-Semag <123025827+Moc-Semag@users.noreply.github.com>
2023-02-06 23:16:29 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -288,7 +288,7 @@ function makeMIDIKeyboard(device) {
 
     mdiv.removeChild(document.getElementById("no-midi-label"));
 
-    const midiNotes = [49, 52, 56, 63];
+    const midiNotes = [49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60, 61, 62, 63];
     midiNotes.forEach(note => {
         const key = document.createElement("div");
         const label = document.createElement("p");
